1545028e56a6a0ca02821e385d8e0a5c5af3c6a0
Yassine Hoummane <136976493+YassineHoummane@users.noreply.github.com>
2026-05-09 19:50:35 +0100

    permissions added

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,19 @@ const nextConfig: NextConfig = {
   },
 };
 export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
